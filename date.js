f428d929b27c985499eba5f18b9aeb4397b058c4
Ajla Saranovic <ge43xed@mytum.de>
2023-09-26 15:14:50 +0200

    date.js

---
+++ date.js
@@ -0,0 +1,5 @@
+export const heroSection = {
+  title: "Home Adventures for Tiny Tinkers",
+  text: "Where smiles, laughter, and creativity come together. Dive into a world of delightful DIY projects, fun activities, and endless inspiration for kids of all ages. Let's embark on a colorful journey of joy and imagination.",
+  img: "https://images.ctfassets.net/hvjzndlg2jsh/7uJxMG09mlDaUKIxpcNX98/102d1b825778f5291942d95cbf19f507/toppng.com-1-preschoolplay-school-for-kids-in-mumbai-kids-play-school-566x677.png",
+};
